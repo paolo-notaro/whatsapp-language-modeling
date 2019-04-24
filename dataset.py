e06d4d25ac8e39330bdd786c9f822b519779a8c2
Paolo Notaro <paulstpr@gmail.com>
2019-04-24 15:20:31 +0200

    modified LabelIndexMap to accept tokens with a predefined index assigned

--- dataset.py
+++ dataset.py
@@ -1,16 +1,35 @@
 from loader import produce_conversations
 import torch
 
-meta_tokens = {"<BEGIN>", "<CHANGE SENDER>", "<NEW MESSAGE>", "<END>"}
+meta_tokens = {"<PAD>", "<BEGIN>", "<CHANGE SENDER>", "<NEW MESSAGE>", "<END>"}
 
 
 class LabelIndexMap(object):
     """
-    class used to remap N labels from cluttered, arbitrary values to non-negative, contiguous values in range [0, N-1].
+    Class used to remap N labels from cluttered, arbitrary values to non-negative, contiguous values in range [0, N-1].
     """
 
-    def __init__(self, all_labels, sort_key=None):
-        self.individual_labels = sorted(set(all_labels), key=sort_key)
+    def __init__(self, all_labels, sort_key=None, required_mappings=None):
+        assert (sort_key is None or required_mappings is None), "use only one among sort_key, predefined_positions"
+        self.individual_labels = list(set(all_labels))
+        if sort_key is not None:
+            self.individual_labels = sorted(all_labels, key=sort_key)
+        if required_mappings is not None:
+            # set items to the required positions
+            for element, required_position in required_mappings.items():
+                if required_position >= len(self.individual_labels):
+                    raise ValueError("Required position is out of range")
+                if self.individual_labels[required_position] in required_mappings:
+                    raise ValueError("Incompatible required mapping: "
+                                     "label {} and {} both required "
+                                     "in position {}".format(element, self.individual_labels[required_position],
+                                                             required_position))
+
+                # swap
+                current_position = self.individual_labels.index(element)
+                self.individual_labels[current_position] = self.individual_labels[required_position]
+                self.individual_labels[required_position] = element
+
         self.label_to_index = {label: self.individual_labels.index(label) for label in self.individual_labels}
         self.index_to_label = {self.individual_labels.index(label): label for label in self.individual_labels}
 
@@ -58,8 +77,8 @@ class WhatsappConversationDataset:
 
 def produce_datasets(whatsapp_export_filepath, val_ratio=0.2):
     conversations = produce_conversations(whatsapp_export_filepath)
-    tokens = set(token for conv in conversations for message in conv for token in message['Tokens']).union(meta_tokens)
-    label_map = LabelIndexMap(tokens)
+    tokens = set(token for conv in conversations for message in conv for token in message['Tokens']) | meta_tokens
+    label_map = LabelIndexMap(tokens, required_mappings={"<PAD>": 0})
     label_map.save("legend.txt")
 
     train_ds_size = int((1 - val_ratio) * len(conversations))
